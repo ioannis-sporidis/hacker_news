a0af25ae021d7364891acdc1747fc55dad2eebe7
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-09-06 00:27:15 -0400

    Displaying stories in Stories component

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -2,13 +2,40 @@ import React from 'react';
 import { useGlobalContext } from '../helpers/context';
 
 const Stories = () => {
-  const { isLoading } = useGlobalContext();
+  const { isLoading, hits } = useGlobalContext();
 
   if (isLoading) {
     return <div className='loading'></div>;
+  } else {
+    return (
+      <section className='stories'>
+        {hits.map(story => {
+          const { objectID, title, num_comments, url, points, author } = story;
+          return (
+            <article key={objectID} className='story'>
+              <h4 className='title'>{title}</h4>
+              <p className='info'>
+                {points} points by <span>{author} | </span> {num_comments}{' '}
+                comments
+              </p>
+              <div>
+                <a
+                  href={url}
+                  className='read-link'
+                  target='_blank'
+                  rel='noreferrer'>
+                  read more
+                </a>
+                <button type='button' className='remove-btn'>
+                  Remove
+                </button>
+              </div>
+            </article>
+          );
+        })}
+      </section>
+    );
   }
-
-  return <div></div>;
 };
 
 export default Stories;
